58cc1ef95a0cef10c84553d34671cfc7d0ae76a8
Chen Kai <281165273grape@gmail.com>
2025-09-02 23:52:58 +0800

    fix:fix wrong put duplicate modules
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- build.zig
+++ build.zig
@@ -2,12 +2,22 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
 
-    const secp256k1 = b.dependency("secp256k1", .{});
+    const secp256k1_dep = b.dependency("secp256k1", .{
+        .target = target,
+        .optimize = optimize,
+    });
 
-    const peer_id = b.dependency("peer_id", .{});
+    const peer_id_dep = b.dependency("peer_id", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const multiformats_dep = peer_id_dep.builder.dependency("zmultiformats", .{
+        .target = target,
+        .optimize = optimize,
+    });
 
     const lib_mod = b.createModule(.{
         .root_source_file = b.path("src/root.zig"),
@@ -15,18 +25,11 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     });
 
-    const multiformats_dep = peer_id.builder.dependency("zmultiformats", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    const multiformats_module = multiformats_dep.module("multiformats-zig");
+    lib_mod.addImport("secp256k1", secp256k1_dep.module("secp256k1"));
+    lib_mod.addImport("peer-id", peer_id_dep.module("peer-id"));
+    lib_mod.addImport("multiformats", multiformats_dep.module("multiformats-zig"));
+    lib_mod.linkLibrary(secp256k1_dep.artifact("libsecp"));
 
-    lib_mod.addImport("secp256k1", secp256k1.module("secp256k1"));
-    lib_mod.addImport("peer-id", peer_id.module("peer-id"));
-    lib_mod.addImport("multiformats", multiformats_module);
-    lib_mod.linkLibrary(secp256k1.artifact("libsecp"));
-
-    try b.modules.put(b.dupe("zig-enr"), lib_mod);
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = "enr",
